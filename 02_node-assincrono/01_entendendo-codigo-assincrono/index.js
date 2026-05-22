3260ce9275ccb07833158c1200c396b7f0182af4
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-05-22 16:08:58 -0300

    Aprendendo o que é um código assíncrono e como o nodejs funciona internamente

---
+++ 02_node-assincrono/01_entendendo-codigo-assincrono/index.js
@@ -0,0 +1,6 @@
+console.log("Primeira linha")
+
+setTimeout(() => {
+    console.log("Segunda linha")
+}, 5000)
+console.log("Terceira linha")
---
+++ 02_node-assincrono/02_nodejs-internamente/index.js
@@ -0,0 +1,12 @@
+console.log("Iniciando ....")
+
+setTimeout(() => {
+    console.log("Temporizador de 3seg ....")
+}, 3000);
+
+setTimeout(() => {
+    console.log("Temporizador de 0seg ....")
+}, 0);
+
+console.log("Finalizando!")
+
